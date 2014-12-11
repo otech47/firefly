f5259912dc15c50cfc4e3c471ed00e13f8bbbb0a
Dan Vera <dan@yovu.co>
2014-12-11 18:34:22 -0500

    assets

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Firefly
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.serve_static_assets = true
+
     config.to_prepare do
       Devise::SessionsController.layout "clean"
       #Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "backend" : "application" }
