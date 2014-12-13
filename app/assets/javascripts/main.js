b75640f67c1f2f8c88975c6f87ecb302ad291186
Dan Vera <dan@yovu.co>
2014-12-13 15:27:21 -0500

    on load change the height of video over layer

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,16 @@
 $(function () {
 
+  setTimeout(function() {
+    var initSize = $('#bgvid').outerHeight();
+    $('#h').css("height", initSize); 
+  }, 1000);
+  
+  $( window ).resize(function() {
+    var videoSize = $('#bgvid').outerHeight();
+
+    $('#h').css("height", videoSize); 
+  });
+
   $('#menuToggle, .menu-close').on('click', function(){
     $('#menuToggle').toggleClass('active');
     $('body').toggleClass('body-push-toleft');
