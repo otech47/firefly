8aeaa9a4196c17709aacba709fccfa0aa511f843
Dan Vera <dan@yovu.co>
2014-12-15 01:38:28 -0500

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,8 +29,6 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.assets.prefix = "/production/assets"
-
   # Generate digests for assets URLs.
   config.assets.digest = true
 
