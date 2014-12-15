a81fa54a3ac03780c83d32ef94ee93f9054cbacb
Dan Vera <dan@yovu.co>
2014-12-15 15:19:30 -0500

    validate form

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,20 +1,38 @@
 $(function () {
 
+  //set initial size
   setTimeout(function() {
     var initSize = $('#bgvid').outerHeight();
     $('#h').css("height", initSize); 
   }, 1000);
   
+  //if resize change size
   $( window ).resize(function() {
     var videoSize = $('#bgvid').outerHeight();
 
     $('#h').css("height", videoSize); 
   });
 
+  //side menu
   $('#menuToggle, .menu-close').on('click', function(){
     $('#menuToggle').toggleClass('active');
     $('body').toggleClass('body-push-toleft');
     $('#theMenu').toggleClass('menu-open');
+  });
+
+  //validate observer registration
+  $("#new_observer").validate({
+    rules: {
+      "observer[email]": {
+        required: true,
+        email: true
+      },
+      "observer[name]": "required"
+    },
+    messages: {
+      "observer[name]": "Please enter your name",
+      "observer[email]": "Please enter a valid email address"
+    }
   });
   
 });
