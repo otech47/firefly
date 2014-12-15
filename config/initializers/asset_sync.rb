7a9037cc0505cfe100d155bdae6fb623c4e32a4a
Dan Vera <dan@yovu.co>
2014-12-15 03:39:47 -0500

    test

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -14,10 +14,10 @@ AssetSync.configure do |config|
   # config.fog_region = 'eu-west-1'
   #
   # Don't delete files from the store
-  # config.existing_remote_files = "keep"
+  config.existing_remote_files = "ignore"
   #
   # Automatically replace files with their equivalent gzip compressed version
-  # config.gzip_compression = true
+  config.gzip_compression = true
   #
   # Use the Rails generated 'manifest.yml' file to produce the list of files to
   # upload instead of searching the assets directory.
