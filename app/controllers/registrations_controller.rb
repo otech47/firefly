b84f805220561cc0b49404d0bdc1cf54e38672db
Dan Vera <dan@yovu.co>
2014-12-18 17:44:11 -0500

    create ticket

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,14 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     land_path
   end
+
+  private
+ 
+    def sign_up_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :btc_address, :ticket)
+    end
+ 
+    def account_update_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :btc_address, :ticket)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141216062054) do
+ActiveRecord::Schema.define(version: 20141218223122) do
 
   create_table "observers", force: true do |t|
     t.string   "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20141216062054) do
     t.string   "name"
     t.string   "repo"
     t.string   "btc_address"
+    t.string   "ticket"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
