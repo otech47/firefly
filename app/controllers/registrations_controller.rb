27d810393755a2204f24be3900e0c90e7a2c3b59
Dan Vera <dan@yovu.co>
2014-12-19 17:33:02 -0500

    after they update profile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
-  def after_sign_up_path_for(resource)
-    land_path
-  end
+    def after_sign_up_path_for(resource)
+      land_path
+    end
 
-  def update_resource(resource, params)
-    resource.update_without_password(params)
-  end
+    def update_resource(resource, params)
+      resource.update_without_password(params)
+    end
+
+    def after_update_path_for(resource)
+      dashboard_path
+    end
 
   private
  
