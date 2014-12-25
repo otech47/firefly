dc3eb71587521eda567564fe3fc149d3fac9a3b9
Dan Vera <dan@yovu.co>
2014-12-25 14:45:07 -0500

    coje turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,12 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require dataTables/jquery.dataTables
 //= require twitter/bootstrap
 //= require_tree .
+//= require turbolinks
 
