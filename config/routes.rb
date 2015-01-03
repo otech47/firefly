1d88a12cc5e9ef9df6d2e0b38b610f9d77f1cf50
Dan Vera <dan@yovu.co>
2015-01-03 16:40:13 -0500

    cameratag

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   namespace :api, :path => "api", :defaults => {:format => :json} do
     namespace :v1 do
       get 'validate/observer' => 'validate#observer'
+      post 'process/cameratag' => 'process#cameratag'
     end
   end
 
