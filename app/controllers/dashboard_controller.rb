8bf6089f13708d8ff6e0421c6a72c18f9ffc8f43
Dan Vera <dan@yovu.co>
2015-01-07 12:13:15 -0500

    add video to profile

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,5 +7,6 @@ class DashboardController < ApplicationController
 
   def profile
   	@hacker = User.find(params[:id])
+    @video = HTTParty.get("http://cameratag.com/api/v4/videos/#{@hacker.team.video}.json?api_key=#{ENV['CAMERATAG_API']}")
   end
 end
