de3f4fe64dc7c2401b44949cb382020770c51298
Dan Vera <dan@yovu.co>
2015-01-09 10:12:47 -0500

    coje sucia

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,11 @@ class ApplicationController < ActionController::Base
       dashboard_path
     end
   end
+
+  private
+
+    # Overwriting the sign_out redirect path method
+    def after_sign_out_path_for(resource_or_scope)
+      new_user_session_path
+    end
 end
