5df2de57679c444937f536fc3c061dbd2fffec4f
Dan Vera <dan@yovu.co>
2015-01-09 10:04:12 -0500

    twitter handle

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
     def after_sign_up_path_for(resource)
-      dashboard_path
+      edit_user_registration_path
     end
 
     def update_resource(resource, params)
@@ -10,16 +10,16 @@ class RegistrationsController < Devise::RegistrationsController
     end
 
     def after_update_path_for(resource)
-      dashboard_path
+      hacker_profile_path(resource.slug)
     end
 
   private
- 
+
     def sign_up_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :btc_address, :ticket, :checked_in, :team_id, :bio)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :btc_address, :ticket, :checked_in, :team_id, :bio, :twitter)
     end
- 
+
     def account_update_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :btc_address, :ticket, :checked_in, :team_id, :bio)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :btc_address, :ticket, :checked_in, :team_id, :bio, :twitter)
     end
 end
