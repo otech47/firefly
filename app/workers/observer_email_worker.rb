599fc9371c2329409dda32b83ea6cd4be8eb34c5
arubinofaux <arubinofaux@gmail.com>
2015-12-09 14:29:12 -0500

    remove workers

--- app/workers/observer_email_worker.rb
+++
@@ -1,9 +0,0 @@
-class ObserverEmailWorker
-  include Sidekiq::Worker
-  sidekiq_options queue: "emails"
-  sidekiq_options retry: true
-  
-  def perform(observer_id)
-    UserMailer.observer_ticket(observer_id).deliver
-  end
-end
